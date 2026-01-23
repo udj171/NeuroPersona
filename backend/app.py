f7ef5843c452414c1c9f62b51fafb5f649e8cb12
udj171 <85997876+udj171@users.noreply.github.com>
2026-01-23 13:44:35 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -136,5 +136,6 @@ def create_app(config_name=None):
     return app
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app = create_app('production')
+    app.run(host='0.0.0.0', port=5000)
+
